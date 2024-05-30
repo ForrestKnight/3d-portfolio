5c6c8f1a15fb04b5dfc818323c4e9a49f20eb6e2
ForrestKnight <forrest.knight.yt@gmail.com>
2024-05-30 11:27:25 -0400

    Actually fixed Hero by adding Position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,7 @@ const Hero = ({ scrollContainer }) => {
           <h1 className='font-medium text-white text-[40px] xs:text-[50px] sm:text-[68px] md:text-[80px] lg:text-[100px] 2xl:text-[180px] leading-[110px] 2xl:leading-[160px]'>
             FORREST KNIGHT
           </h1>
+          <Position />
         </div>
         <div className="flex-1 flex justify-start lg:justify-end mt-4 sm:mt-14 ml-8 xs:ml-[-4vh] sm:ml-[-17vh] md:ml-[-26vh] lg:mt-10 2xl:mt-0">
           <div className='font-bold text-[20px] sm:text-[30px] md:text-[36px] 2xl:text-[46px] sm:leading-[40px] md:leading-[50px] 2xl:leading-[60px] streaky-glow max-w-sm 2xl:max-w-lg text-white text-left'>
@@ -16,7 +17,7 @@ const Hero = ({ scrollContainer }) => {
           </div>
         </div>
       </div>
-      <Position />
+
       <img className="parallax__stars" src="./parallax/1Stars.svg" alt="" />
       <img className="parallax__planets" src="./parallax/2Planets.svg" alt="" />
       <img className="parallax__mountain1" src="./parallax/3Mountain.svg" alt="" />
